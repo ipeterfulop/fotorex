4711bcfbcddd60cf043213d6485dc8410040cbb9
OlahTamas <olah.tamas@datalytix.tech>
2020-09-23 04:20:28 +0000

    Added variable name to attributes

--- database/seeds/PopulateAttributesTable.php
+++ database/seeds/PopulateAttributesTable.php
@@ -33,6 +33,7 @@ class PopulateAttributesTable extends Seeder
             'name',
             'label',
             'is_computed',
+            'variable_name',
             'created_at',
             'updated_at',
         ];
@@ -42,15 +43,15 @@ class PopulateAttributesTable extends Seeder
     private function getRawDataSet()
     {
         $dataSet = [
-            ['id' => 1, 'attribute_value_set_id' => 3, 'name' => 'Nyomtatás', 'label' => 'Print'],
-            ['id' => 2, 'attribute_value_set_id' => 3, 'name' => 'Másolás', 'label' => 'Copy'],
-            ['id' => 3, 'attribute_value_set_id' => 3, 'name' => 'Szkennelés', 'label' => 'Scan'],
-            ['id' => 4, 'attribute_value_set_id' => 1, 'name' => 'Fax', 'label' => null],
-            ['id' => 5, 'attribute_value_set_id' => null, 'name' => 'Nyomtatási méret (max)', 'is_computed' => 1],
-            ['id' => 6, 'attribute_value_set_id' => null, 'name' => 'Hardveres felbontás'],
-            ['id' => 7, 'attribute_value_set_id' => null, 'name' => 'Sebesség fekete-fehér (A4)'],
-            ['id' => 8, 'attribute_value_set_id' => null, 'name' => 'Sebesség fekete-fehér (A3)'],
-            ['id' => 9, 'attribute_value_set_id' => 5, 'name' => 'Hálózatos / Helyi'],
+            ['id' => 1, 'variable_name' => 'printing', 'attribute_value_set_id' => 3, 'name' => 'Nyomtatás', 'label' => 'Print'],
+            ['id' => 2, 'variable_name' => 'copying', 'attribute_value_set_id' => 3, 'name' => 'Másolás', 'label' => 'Copy'],
+            ['id' => 3, 'variable_name' => 'scanning', 'attribute_value_set_id' => 3, 'name' => 'Szkennelés', 'label' => 'Scan'],
+            ['id' => 4, 'variable_name' => 'faxing', 'attribute_value_set_id' => 1, 'name' => 'Fax', 'label' => null],
+            ['id' => 5, 'variable_name' => 'papersize', 'attribute_value_set_id' => null, 'name' => 'Nyomtatási méret (max)', 'is_computed' => 1],
+            ['id' => 6, 'variable_name' => 'printing_resolution', 'attribute_value_set_id' => null, 'name' => 'Hardveres felbontás'],
+            ['id' => 7, 'variable_name' => 'printing_speed_a4', 'attribute_value_set_id' => null, 'name' => 'Sebesség fekete-fehér (A4)'],
+            ['id' => 8, 'variable_name' => 'printing_speed_a3', 'attribute_value_set_id' => null, 'name' => 'Sebesség fekete-fehér (A3)'],
+            ['id' => 9, 'variable_name' => 'networked', 'attribute_value_set_id' => 5, 'name' => 'Hálózatos / Helyi'],
 
         ];
         /**
