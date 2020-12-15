324a7cfebcf3ecf6cfc2d5685dafab169ec63c29
OlahTamas <olah.tamas@datalytix.tech>
2020-12-15 10:20:36 +0000

    Slider items

--- resources/views/public/partials/highlightedprinter.blade.php
+++ resources/views/public/partials/highlightedprinter.blade.php
@@ -1,11 +1,11 @@
-<div class="pr-10 h-100 my-4 pr-8">
+<div class="pr-10 h-128 my-4 pr-8">
     <a href="{{ $item->printer->getDetailsUrl() }}" class="hover:shadow-lg  w-full h-full  flex flex-col items-stretch justify-start p-2 relative  border border-fotolightgray  hover:shadow-md">
         <img src="{{ $item->printer->getMainImageUrl(request()->get('printerphotoroles')->get('index')) }}" class="object-cover" style="height: 50%">
         <div class="flex flex-col items-stretch justify-start mt-3">
             <h4 class="text-fotored uppercase text-xl" >
                 {{ $item->printer->shortdisplayname }}<span class="italic font-normal ml-2 text-sm">{{ $item->printer->name }}</span>
             </h4>
-            <div class="text-sm overflow-y-hidden relative" style="max-height: 3rem;">
+            <div class="text-sm overflow-y-hidden relative">
                 {{ $item->printer->key_features_label }}
             </div>
         </div>
